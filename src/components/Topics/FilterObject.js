49f7077cf93dbb32f6218043474eff7711c08748
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2020-08-18 23:08:47 -0500

    finished step 4

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,56 @@
 import React, {Component} from 'react';
 
-export default class FilterObject extends Component{
+export default class FilterObject extends Component {
+    constructor() {
+        super();
+        this.state = {
+            characters: [
+                {
+                    job: 'ninja',
+                    rarity: 6,
+                    role: 'dps'
+                },
+                {
+                    job: 'Knight',
+                    role: 'tank',
+                    ability: 'taunt'
+                },
+                {
+                    job: 'Bard',
+                    rarity: 3,
+                }
+            ],
+            userInput: '',
+            filteredCharacters: []
+        }
+    }
+
+    handleChange(val){
+        this.setState({ userInput: val });
+    }
+
+    filterCharacters(prop){
+        let characters = this.state.characters;
+        let filteredCharacters = [];
+
+        for ( let i=0; i < characters.length; i++ ) {
+            if( characters[i].hasOwnProperty(prop) ) {
+                filteredCharacters.push(characters[i]);
+            }
+        }
+
+        this.setState({ filteredCharacters: filteredCharacters });
+    }
+
     render(){
         return(
-            <p> FilterObject Component </p>
+            <div className='puzzleBox filterObjectPB'>
+                <h4> Filter Object </h4>
+                <span className='puzzleText'> Original: {JSON.stringify(this.state.characters, null, 10) }</span>
+                <input className='inputLine' onChange={ (e) => this.handleChange(e.target.value) }/>
+                <button className='confirmationButton' onClick={ () => this.filterCharacters(this.state.userInput)}> Filter </button>
+                <span className='resultsBox filterObjectPB'> Filtered: { JSON.stringify(this.state.filteredCharacters, null, 10) } </span>
+            </div>
         )
     }
 }
